d5d28c40fdd8f30c129c831714e3aea22e0525cd
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2018-06-28 17:35:53 +0530

    Add files via upload

---
+++ XV6 IPC implementations/ruchir_program.c
@@ -0,0 +1,10 @@
+// A simple program which just prints something on screen
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+ printf(1, "RUCHIR first user program on xv6\n");
+ exit();
+}
